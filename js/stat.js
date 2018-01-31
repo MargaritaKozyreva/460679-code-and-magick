853e7ca2c927e09fac8554251d51f69c49cd459a
Маргарита <margaritakozyreva@mail.ru>
2018-02-01 00:32:02 +0300

    Код исправлен

--- js/stat.js
+++ js/stat.js
@@ -1,52 +1,69 @@
+"use strict";
+
 window.renderStatistics = function(ctx, names, times) {
-    var cloud_x = 100;
-    var cloud_y = 10;
-    var cloud_weight = 420;
-    var cloud_height = 270;
-    var font_text = "16px PT Mono";
 
-    ctx.fillStyle = "rgba(0, 0, 0, 0.7)"; //рисуем тень области
-    ctx.fillRect(cloud_x + 10, cloud_y + 10, cloud_weight, cloud_height); //рисуем смещение тени области
-    ctx.fillStyle = "white"; //рисуем background области
-    ctx.fillRect(cloud_x, cloud_y, cloud_weight, cloud_height); //выводим область
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var CLOUD_WEIGHT = 420;
+var CLOUD_HEIGHT = 270;
+var FONT_TEXT = "16px PT Mono";
+var COLOR_TEXT = "black";
+var CLOUD_BG = "white";
+var CLOUD_SHADOW = "rgba(0, 0, 0, 0.7)";
+var CLOUD_CHANGE = 10;
 
-    var max_pers = 0; //находим максимальное значение баллов
-    for (var k = 0; k < times.length; k++) {
-        if (times[k] > max_pers) {
-            max_pers = times[k];
-        }
-    }
+var maxPercent = 100;
+var maxResult = 0;
+var isTopResult = false;
 
-    var max_persent = 100; // 100 - максимальный процент, от которого будем отталкиваться при выводе шкал гистограммы;
-    var top_result = false; //маркер, с помощью которого будем менять текст результата в зависимости набрала ли я макс. балл или нет;
+function getCloud (color, position, x , y) {
+ctx.fillStyle = color;
+ctx.fillRect(x + position, y + position, CLOUD_WEIGHT, CLOUD_HEIGHT );
+}
 
-    for (var i = 0; i < names.length; i++) { //циклом выводим имена в область
-        ctx.fillStyle = "black";
-        ctx.font = font_text;
-        ctx.fillText(names[i], cloud_x + 35 + (i * 90), 265);
+getCloud("rgba(0, 0, 0, 0.7)", CLOUD_CHANGE, CLOUD_X, CLOUD_Y);
+getCloud(CLOUD_BG , 0 , CLOUD_X, CLOUD_Y);
 
-    }
-    for (var j = 0; j < times.length; j++) { //  делаем расчеты для отрисовки гистограммы
-        var max_pers_length = ((times[j] * max_persent) / max_pers);
-        ctx.fillStyle = "rgba(0, 0, 255," + Math.random() + ")";
-        if (names[j] === 'Вы') { // проверка на себя и окрашивание в определенный цвет
-            ctx.fillStyle = "rgba(255, 0, 0, 1)";
+var getMaxElement = function(arr) {
+var maxElement = arr[0];
 
-            if (times[j] === max_pers) {
-               top_result = true;
-            }
-        }
+for (var i = 0; i < arr.length; i++) {
+if (arr[i] > maxElement) {
+maxElement = arr[i];
+}
+}
 
-        ctx.fillRect(cloud_x + 35 + (j * 90), 240 - (max_pers_length * 1.5), 40, max_pers_length * 1.5); // задаем параметры гистограммы
-        ctx.fillText(Math.round(times[j]), cloud_x + 35 + (j * 90), 230 - (max_pers_length * 1.5)) // выводим кол-во баллов сверху шкалы
-    }
+return maxElement;
+};
 
-    ctx.fillStyle = "black";
-    ctx.font = font_text;
-    if (top_result) {
-        ctx.fillText("Ура вы победили! У вас максимум!", 130, 40);
-    } else {
-        ctx.fillText("Ура вы победили! Но максимум не ваш", 130, 40);
-    }
-    ctx.fillText("Список результатов:", 130, 55);
+maxResult = getMaxElement(times);
+var histSpace = 0;
+for (var i = 0; i < names.length; i++) {
+histSpace = CLOUD_X + 35 + (i * 90);
+ctx.fillStyle = COLOR_TEXT;
+ctx.font = FONT_TEXT;
+ctx.fillText(names[i], histSpace, 265);
+var histPersLength = ((times[i] * maxPercent) / maxResult);
+ctx.fillStyle = "rgba(0, 0, 255," + Math.random() + ")";
+if (names[i] === "Вы") {
+ctx.fillStyle = "rgba(255, 0, 0, 1)";
+
+if (times[i] === maxResult) {
+isTopResult = true;
+}
+}
+
+var histMaxLength = histPersLength * 1.5;
+ctx.fillRect(histSpace, 240 - (histMaxLength), 40, histMaxLength);
+ctx.fillText(Math.round(times[i]), histSpace, 230 - (histMaxLength));
+}
+
+ctx.fillStyle = COLOR_TEXT;
+ctx.font = FONT_TEXT;
+if (isTopResult) {
+ctx.fillText("Ура вы победили! У вас максимум!", 130, 40);
+} else {
+ctx.fillText("Ура вы победили! Но максимум не ваш", 130, 40);
+}
+ctx.fillText("Список результатов:", 130, 55);
 }
