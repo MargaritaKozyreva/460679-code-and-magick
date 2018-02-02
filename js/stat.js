15f5238b809e6f7bb1c2495de14572a22575d224
Маргарита <margaritakozyreva@mail.ru>
2018-02-02 09:48:12 +0300

    Изменение кода в соответствии с комментариями наставника

--- js/stat.js
+++ js/stat.js
@@ -1,68 +1,75 @@
-"use strict";
+'use strict';
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
 
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var CLOUD_WEIGHT = 420;
-var CLOUD_HEIGHT = 270;
-var FONT_TEXT = "16px PT Mono";
-var COLOR_TEXT = "black";
-var CLOUD_BG = "white";
-var CLOUD_SHADOW = "rgba(0, 0, 0, 0.7)";
-var CLOUD_CHANGE = 10;
+  var CLOUD_X = 100;
+  var CLOUD_Y = 10;
+  var CLOUD_WIDTH = 420;
+  var CLOUD_HEIGHT = 270;
+  var FONT_TEXT = '16px PT Mono';
+  var COLOR_TEXT = 'black';
+  var CLOUD_BG = 'white';
+  var CLOUD_SHADOW = 'rgba(0, 0, 0, 0.7)';
+  var CLOUD_CHANGE_POSITION = 10;
 
-var maxPercent = 100;
-var maxResult = 0;
-var isTopResult = false;
 
-function getCloud (color, position, x , y) {
- ctx.fillStyle = color;
- ctx.fillRect(x + position, y + position, CLOUD_WEIGHT, CLOUD_HEIGHT );
-}
+  function renderRectangle(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+  }
 
-getCloud("rgba(0, 0, 0, 0.7)", CLOUD_CHANGE, CLOUD_X, CLOUD_Y);
-getCloud(CLOUD_BG , 0 , CLOUD_X, CLOUD_Y);
+  function renderText(text, x, y, font, color) {
+    ctx.font = font;
+    ctx.fillStyle = color;
+    ctx.fillText(text, x, y);
+  }
 
-var getMaxElement = function(arr) {
-var maxElement = arr[0];
+  renderRectangle(
+      CLOUD_X + CLOUD_CHANGE_POSITION, CLOUD_Y + CLOUD_CHANGE_POSITION, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_SHADOW);
 
-for (var i = 0; i < arr.length; i++) {
- if (arr[i] > maxElement) {
- maxElement = arr[i];
-}
-}
-return maxElement;
+  renderRectangle(
+      CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_BG);
+
+  var getMaxElement = function (arr) {
+    var maxElement = -1;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] > maxElement) {
+        maxElement = arr[i];
+      }
+    }
+    return maxElement;
+  };
+
+  var MAX_PERCENT = 100;
+  var maxResult = 0;
+  var isTopResult = false;
+
+  maxResult = getMaxElement(times);
+  var INITIAL_TEXT_NAME_Y = 265;
+  for (var i = 0; i < names.length; i++) {
+    var initialX = CLOUD_X + 35 + (i * 90);
+    renderText(names[i], initialX, INITIAL_TEXT_NAME_Y, FONT_TEXT, COLOR_TEXT);
+
+
+    var histPersLength = ((times[i] * MAX_PERCENT) / maxResult);
+    var color;
+    color = "rgba(0, 0, 255," + Math.random() + ")";
+    if (names[i] === 'Вы') {
+      color = 'rgba(255, 0, 0, 1)';
+      if (times[i] === maxResult) {
+        isTopResult = true;
+      }
+    }
+
+    var histMaxLength = histPersLength * 1.5;
+    renderRectangle(initialX, 240 - (histMaxLength), 40, histMaxLength, color);
+    renderText(Math.round(times[i]), initialX, 230 - (histMaxLength), FONT_TEXT, color);
+  }
+
+  if (isTopResult) {
+    renderText('Ура вы победили! У вас максимум!', 130, 40, FONT_TEXT, COLOR_TEXT);
+  } else {
+    renderText('Ура вы победили! Но максимум не ваш!', 130, 40, FONT_TEXT, COLOR_TEXT);
+  }
+  renderText('Список результатов:', 130, 55, FONT_TEXT, COLOR_TEXT);
 };
-
-maxResult = getMaxElement(times);
-var histSpace = 0;
-for (var i = 0; i < names.length; i++) {
- histSpace = CLOUD_X + 35 + (i * 90);
- ctx.fillStyle = COLOR_TEXT;
- ctx.font = FONT_TEXT;
- ctx.fillText(names[i], histSpace, 265);
- var histPersLength = ((times[i] * maxPercent) / maxResult);
- ctx.fillStyle = "rgba(0, 0, 255," + Math.random() + ")";
-  if (names[i] === "Вы") {
-   ctx.fillStyle = "rgba(255, 0, 0, 1)";
-
-  if (times[i] === maxResult) {
-   isTopResult = true;
-}
-}
-
-var histMaxLength = histPersLength * 1.5;
-ctx.fillRect(histSpace, 240 - (histMaxLength), 40, histMaxLength);
-ctx.fillText(Math.round(times[i]), histSpace, 230 - (histMaxLength));
-}
-
-ctx.fillStyle = COLOR_TEXT;
-ctx.font = FONT_TEXT;
-if (isTopResult) {
- ctx.fillText("Ура вы победили! У вас максимум!", 130, 40);
- } else {
- ctx.fillText("Ура вы победили! Но максимум не ваш", 130, 40);
-}
-ctx.fillText("Список результатов:", 130, 55);
-}
